5b9a9a7b1e9e3aa5598c652db89e89b51ed33562
lourdes <lourdesgeaneth@gmail.com>
2024-05-26 04:02:41 -0500

    Agregando Contenido

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -8,8 +8,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using silliconValley.Data;
 using silliconValley.Integration.jsonplaceholder;
 using silliconValley.Integration.jsonplaceholder.dto;
+using silliconValley.Models;
 
 
 
@@ -18,5 +20,38 @@ namespace silliconValley.Controllers
    
     public class UsuarioController : Controller
     {
+        private readonly ILogger<UsuarioController> _logger;
+        private object apiUsers;
+        private readonly ApplicationDbContext _context;
+
+        public UsuarioController(ILogger<UsuarioController> logger,ApplicationDbContext context)
+        {
+            _logger = logger;
+                       _context = context;
+        }
+         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnviarMensaje(ApiUser objapiuser)
+        {
+            _logger.LogDebug("Ingreso a Enviar Mensaje");
+
+            //Se registran los datos del objeto a la base datos
+            _context.Add(objapiuser);
+            _context.SaveChanges();
+
+            ViewData["Message"] = "Se registro el Usuario";
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
     }
 }
+    
